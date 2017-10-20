64d3a3d66965c793d2a242a1aacd8d44d3e91531
Akira Komamura <akira.komamura@gmail.com>
2017-10-20 10:35:11 +0900

    Let deft ignore files under node_modules

--- layers/deft-extras/config.el
+++ layers/deft-extras/config.el
@@ -1,3 +1,4 @@
 (setq deft-recursive t
       deft-use-filename-as-title nil
-      deft-use-filter-string-for-filename t)
+      deft-use-filter-string-for-filename t
+      deft-recursive-ignore-dir-regexp "\\(?:\\.\\|\\.\\.\\|node_modules\\)$")
