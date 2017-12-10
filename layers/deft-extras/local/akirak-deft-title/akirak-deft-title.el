8f55262ae0e1b812e3c0674ddcf1c0ddeb8b40a8
Akira Komamura <sam7wx@gmx.com>
2017-12-10 19:04:21 +0900

    Renamed my-deft-title to akirak-deft-title

--- layers/deft-extras/local/akirak-deft-title/akirak-deft-title.el
+++ layers/deft-extras/local/akirak-deft-title/akirak-deft-title.el
@@ -1,39 +1,39 @@
-(defun my-deft/strip-quotes (str)
+(defun akirak/deft-strip-quotes (str)
   (cond ((string-match "\"\\(.+\\)\"" str) (match-string 1 str))
         ((string-match "'\\(.+\\)'" str) (match-string 1 str))
         (t str)))
 
-(defun my-deft/parse-title-from-front-matter-data (str)
+(defun akirak/deft-parse-title-from-front-matter-data (str)
   (if (string-match "^title: \\(.+\\)" str)
-      (let* ((title-text (my-deft/strip-quotes (match-string 1 str)))
+      (let* ((title-text (akirak/deft-strip-quotes (match-string 1 str)))
              (is-draft (string-match "^draft: true" str)))
         (concat (if is-draft "[DRAFT] " "") title-text))))
 
-(defun my-deft/deft-file-relative-directory (filename)
+(defun akirak/deft-deft-file-relative-directory (filename)
   (file-name-directory (file-relative-name filename deft-directory)))
 
-(defun my-deft/title-prefix-from-file-name (filename)
-  (let ((reldir (my-deft/deft-file-relative-directory filename)))
+(defun akirak/deft-title-prefix-from-file-name (filename)
+  (let ((reldir (akirak/deft-deft-file-relative-directory filename)))
     (if reldir
         (concat (directory-file-name reldir) " > "))))
 
-(defun my-deft/parse-title-with-directory-prepended (orig &rest args)
+(defun akirak/deft-parse-title-with-directory-prepended (orig &rest args)
   (let ((str (nth 1 args))
         (filename (car args)))
     (concat
-      (my-deft/title-prefix-from-file-name filename)
+      (akirak/deft-title-prefix-from-file-name filename)
       (let ((nondir (file-name-nondirectory filename)))
         (if (or (string-prefix-p "README" nondir)
                 (string-suffix-p ".txt" filename))
             nondir
           (if (string-prefix-p "---\n" str)
-              (my-deft/parse-title-from-front-matter-data
+              (akirak/deft-parse-title-from-front-matter-data
                (car (split-string (substring str 4) "\n---\n")))
             (apply orig args)))))))
 
-(defun my-deft/parse-title (filename contents)
+(defun akirak/deft-parse-title (filename contents)
   (concat
-   (my-deft/title-prefix-from-file-name filename)
+   (akirak/deft-title-prefix-from-file-name filename)
    (let ((nondir (file-name-nondirectory filename)))
      (cond
       ;; If the filename is README.* or *.txt, return the file name as it is
@@ -42,7 +42,7 @@
        nondir)
       ;; If the content contains a YAML front matter, extract title from it
       ((string-prefix-p "---\n" contents)
-       (my-deft/parse-title-from-front-matter-data
+       (akirak/deft-parse-title-from-front-matter-data
         (car (split-string (substring contents 4) "\n---\n"))))
       (t
        (let ((begin (string-match "^.+$" contents)))
@@ -52,4 +52,4 @@
            nondir))
       )))))
 
-(provide 'my-deft-title)
+(provide 'akirak-deft-title)
--- layers/deft-extras/packages.el
+++ layers/deft-extras/packages.el
@@ -1,11 +1,11 @@
 (setq deft-extras-packages
       '(
-        (my-deft-title :location local)
+        (akirak-deft-title :location local)
         ))
 
-(defun deft-extras/init-my-deft-title ()
-  (use-package my-deft-title))
+(defun deft-extras/init-akirak-deft-title ()
+  (use-package akirak-deft-title))
 
-(defun deft-extras/post-init-my-deft-title ()
-  (advice-add 'deft-parse-title :override #'my-deft/parse-title)
+(defun deft-extras/post-init-akirak-deft-title ()
+  (advice-add 'deft-parse-title :override #'akirak/deft-parse-title)
   )
